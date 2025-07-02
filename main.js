8dd38ab0562d9345e61ef5fd00d84686fadf2c45
Professor Aílson <102998807+Prof-Ailson@users.noreply.github.com>
2025-07-02 19:40:34 -0300

    0207

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ function inserirNome (){
 }
 
 inserirNome();
+
+
+let linguagens = ["JavaScript", "Python","C++"];
+
+//console.log(linguagens)[1];
+
+const item = document.querySelector("#lista");
+//item.textContent = linguagens[0] + ' - ' +  linguagens[1] + ' - ' +  linguagens[2];
+item.textContent = linguagens; 
